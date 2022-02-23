6b6ad70b179d24ef2c1393176fd7185543922f9d
Venkatesha-Iyengar <90900979+Venkatesha-Iyengar@users.noreply.github.com>
2022-02-23 09:26:44 +0530

    Add files via upload

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,3 +1,13 @@
+coolingTypes ={
+    'PASSIVE_COOLING' : [0, 35],
+    'HI_ACTIVE_COOLING' : [0, 45],
+    'MED_ACTIVE_COOLING' : [0, 40],
+    }
+
+email_content = {
+    'TOO_LOW' : 'Hi, the temperature is too low',
+    'TOO_HIGH' : 'Hi, the temperature is too high',
+    }
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
@@ -6,41 +16,35 @@ def infer_breach(value, lowerLimit, upperLimit):
     return 'TOO_HIGH'
   return 'NORMAL'
 
-
+def define_temperature_breach_limits(coolingType):
+    return (coolingTypes.get(coolingType, [0,0]))
+  
 def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
+  lowerLimit, upperLimit = define_temperature_breach_limits(coolingType)
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
 
+def alertTargets(alertTarget, breachType):
+    return {
+        'TO_CONTROLLER': lambda: send_to_controller(breachType),
+        'TO_EMAIL': lambda: send_to_email(breachType),
+    }.get(alertTarget,lambda: 'Not Valid')()
 
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
+  flag_alerted = False
   breachType =\
     classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-
+  flag_alerted = alertTargets(alertTarget, breachType)
+  return flag_alerted
 
 def send_to_controller(breachType):
   header = 0xfeed
   print(f'{header}, {breachType}')
+  return True
 
+def email_body(breachType, recepient):
+    return f'To: {recepient} \n{email_content.get(breachType, "Invalid Breach Type")}'
 
 def send_to_email(breachType):
   recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+  print(email_body(breachType, recepient))
+  return True
--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,11 +1,32 @@
 import unittest
 import typewise_alert
+import random
 
+global CoolingTypes
+CoolingTypes = {
+  'PASSIVE_COOLING' :[0,35],
+  'HI_ACTIVE_COOLING' : [0,45],
+  'MED_ACTIVE_COOLING' : [0,40]
+  }
 
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+    global CoolingTypes
+    for cooling_type in CoolingTypes.keys():
+      batteryChar= dict()
+      batteryChar['coolingType'] = cooling_type
 
+      lower_limit, upper_limit = CoolingTypes[cooling_type]
+      
+      self.assertTrue(typewise_alert.classify_temperature_breach(cooling_type, lower_limit-1) == 'TOO_LOW')
+      self.assertTrue(typewise_alert.classify_temperature_breach(cooling_type, upper_limit+1) == 'TOO_HIGH')
+      self.assertTrue(typewise_alert.classify_temperature_breach(cooling_type, lower_limit) == 'NORMAL')
+      self.assertTrue(typewise_alert.classify_temperature_breach(cooling_type, upper_limit) == 'NORMAL')
+      self.assertTrue(typewise_alert.classify_temperature_breach(cooling_type, random.randrange(lower_limit, upper_limit)) == 'NORMAL')
 
+      self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', batteryChar, random.randrange(lower_limit, upper_limit )))
+      self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', batteryChar, random.randrange(lower_limit, upper_limit)))
+      self.assertTrue(typewise_alert.check_and_alert('TO_Somewhere', batteryChar, random.randrange(lower_limit, upper_limit)) == 'Not Valid')
+      
 if __name__ == '__main__':
   unittest.main()
